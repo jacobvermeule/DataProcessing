6f7d324778767995ec457c9dfca482db69532320
jacobvermeule <jacobvermeule@gmail.com>
2018-11-05 18:49:19 +0100

    Update 1

--- Homework/Week_1/moviescraper.py
+++ Homework/Week_1/moviescraper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# Name:
-# Student number:
+# Name: Jacob Vermeule
+# Student number: 11328622
 """
 This script scrapes IMDB and outputs a CSV file with highest rated movies.
 """
@@ -10,7 +10,7 @@ from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
-
+soup = BeautifulSoup
 TARGET_URL = "https://www.imdb.com/search/title?title_type=feature&release_date=2008-01-01,2018-01-01&num_votes=5000,&sort=user_rating,desc"
 BACKUP_HTML = 'movies.html'
 OUTPUT_CSV = 'movies.csv'
@@ -27,12 +27,54 @@ def extract_movies(dom):
     - Runtime (only a number!)
     """
 
+    # initiate list for titles
+    titles = []
+
+    # open html
+    html_file = open("movies.html", 'r')
+    page = soup(html_file.read(), features="html.parser")
+
+    # skip over first two titles
+    itermovie = iter(page.find_all("img"))
+    next(itermovie)
+    next(itermovie)
+
+    # append movietitles to list
+    for movie in itermovie:
+        titles.append(movie.get("alt"))
+
+    # initiate ratings
+    ratings = []
+
+    # find all ratings and append to list
+    for movie in page.find_all("div"):
+        if movie.get("data-value") != None:
+            ratings.append(movie.get("data-value"))
+
+    # initiate year of release list
+    year_of_release = []
+
+    # go to location of years of release
+    for movie in page.find_all("span"):
+        if movie.get("class") == ['lister-item-year', 'text-muted', 'unbold']:
+            for i in movie:
+                for j in i:
+
+                    # check if year is found
+                    if j.isdigit():
+                        year_of_release.append(j)
+
+    # make new list with combined digits
+    years_of_release = []
+    for i in range(0, len(year_of_release), 4):
+        years_of_release.append(year_of_release[i] + year_of_release[i+1] + year_of_release[i+2] + year_of_release[i+3])
+    
     # ADD YOUR CODE HERE TO EXTRACT THE ABOVE INFORMATION ABOUT THE
     # HIGHEST RATED MOVIES
     # NOTE: FOR THIS EXERCISE YOU ARE ALLOWED (BUT NOT REQUIRED) TO IGNORE
     # UNICODE CHARACTERS AND SIMPLY LEAVE THEM OUT OF THE OUTPUT.
 
-    return []   # REPLACE THIS LINE AS WELL IF APPROPRIATE
+    return titles   # REPLACE THIS LINE AS WELL IF APPROPRIATE
 
 
 def save_csv(outfile, movies):
