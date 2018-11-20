def6fd2b4d1f650c95c34221c0b614c893280faf
jacobvermeule <jacobvermeule@gmail.com>
2018-11-20 13:58:12 +0100

    Tussentijds opslaanbeurtje

---
+++ Homework/Week_3/convertCSV2JSON.py
@@ -0,0 +1,21 @@
+# Jacob Vermeule 11328622
+# Minor Programmeren 2018
+# University of Amsterdam
+# kijk alles is even lang
+
+import csv
+import json
+
+
+# Open the CSV
+f = open('Homework\Week_3\KNMI.csv', 'r')
+
+# Change column names
+reader = csv.DictReader(f, fieldnames = ("Station","Date","Temperature"))
+
+# Parse the CSV into JSON
+out = json.dumps(reader)
+
+# Save the JSON
+f = open('Homework\Week_3\KNMI.json', 'w')
+f.write(out)
--- README.md
+++
@@ -1,4 +0,0 @@
-# DataProcessing
-#  Jacob Vermeule 11328622
-#  University of Amsterdam
-# Minor Programming
